f668cd23881e1e6c21513c490bfb56bdbd570e32
Alganduro Coder <algandurocoding@gmail.com>
2015-09-08 22:04:47 +0100

    Initial version. Too sleepy to finish.

---
+++ MarsRover/MarsRover/Rover.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    class Rover
+    {
+        public RoverPosition Position { get; set; }
+        public Area Plateau { get; set; }
+
+        public Rover(Area plateau, RoverPosition initialPosition)
+        {
+            Plateau = plateau;
+            Position = initialPosition;
+        }
+
+        public void ProcessInput(string inputCommand)
+        {
+            foreach (char command in inputCommand)
+                ProcessCommand(command);
+        }
+
+        private void ProcessCommand(char command)
+        {
+            switch( command)
+            {
+                case 'L':
+                    TurnLeft();
+                    break;
+                case 'R':
+                    TurnRight();
+                    break;
+                case 'M':
+                    Move();
+                    break;
+            }
+        }
+
+        private void TurnLeft()
+        {
+            if (Position.Heading == RoverPosition.Direction.N)
+                Position.Heading = RoverPosition.Direction.W;
+            else
+            {
+                Position.Heading -= 1;
+            }
+        }
+
+        private void TurnRight()
+        {
+            if (Position.Heading == RoverPosition.Direction.W)
+                Position.Heading = RoverPosition.Direction.N;
+            else
+            {
+                Position.Heading += 1;
+            }
+        }
+
+        private void Move()
+        {
+            switch(Position.Heading)
+            {
+                case RoverPosition.Direction.N:
+                    if (Position.Y < Plateau.Height)
+                        Position.Y += 1;
+                    break;
+                case RoverPosition.Direction.E:
+                    if (Position.X < Plateau.Width)
+                        Position.X += 1;
+                    break;
+                case RoverPosition.Direction.S:
+                    if (Position.Y > 0)
+                        Position.Y -= 1;
+                    break;
+                case RoverPosition.Direction.W:
+                    if (Position.X > 0)
+                        Position.X -= 1;
+                    break;
+            }
+        }
+
+    }
+}
---
+++ MarsRover/MarsRover/RoverPosition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    class RoverPosition
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Direction Heading { get; set; }
+
+        public enum Direction
+        {
+            N=0,
+            E=1,
+            S=2,
+            W=3
+        }
+    }
+}
